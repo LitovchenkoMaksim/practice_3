afe5b78807ff4639f6f60a0946e9fe35938d34ec
Maksim <90028538+LitovchenkoMaksim@users.noreply.github.com>
2021-10-09 16:55:51 +0300

    Create task_3.py

---
+++ task_3.py
@@ -0,0 +1,10 @@
+a = int(input("Введите число:"))
+if a % 5 == 0 and a % 7 == 0:
+    print("11")
+else:
+    if a % 5 == 0:
+        print("01")
+    elif a % 7 == 0:
+        print("10")
+    else:
+        print("00")
